e93ab29cc81b0ed01302182696c1ddbdef6b43b5
wurzi200 <amjordan07@web.de>
2023-09-28 15:13:15 +0200

    add react-icons and add delete Button style with icon

--- resources/js/Pages/Todos/todo.jsx
+++ resources/js/Pages/Todos/todo.jsx
@@ -3,6 +3,10 @@ import SecondaryButton from "@/Components/SecondaryButton";
 import { router } from "@inertiajs/react";
 import axios from "axios";
 
+import { BiTrash } from "react-icons/bi";
+
+
+
 export default function Todo({ todo }) {
 
   function deleteTodo() {
@@ -21,10 +25,13 @@ export default function Todo({ todo }) {
 
   return (
     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg m-3 flex">
-      <div className="p-6 text-gray-900 w-full">{todo.description}</div>
+      <div className="p-6 text-gray-900 w-full overflow-hidden">{todo.description}</div>
       <SecondaryButton
+        className={"m-4 border-none shadow-none"}
         onClick={() => deleteTodo()}
-      />
+      >
+        <BiTrash className="text-2xl" />
+      </SecondaryButton>
     </div>
   );
 }
