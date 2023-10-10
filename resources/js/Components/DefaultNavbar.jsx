5e1418a3331b880351c37c5068adae22be8aae32
wurzi200 <amjordan07@web.de>
2023-10-10 16:13:31 +0200

    change sidebartheme and routes

--- resources/js/Components/DefaultNavbar.jsx
+++ resources/js/Components/DefaultNavbar.jsx
@@ -1,11 +1,11 @@
 'use client';
 
-import { Link } from '@inertiajs/react';
-import { Dropdown, Navbar, Avatar } from 'flowbite-react';
-import ApplicationLogo from './ApplicationLogo';
-import NavLink from './NavLink';
 import { useState } from 'react';
-import ResponsiveNavLink from './ResponsiveNavLink';
+import ApplicationLogo from '@/Components/ApplicationLogo';
+import Dropdown from '@/Components/Dropdown';
+import NavLink from '@/Components/NavLink';
+import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
+import { Link } from '@inertiajs/react';
 
 export default function NavbarWithDropdown({ auth, user }) {
   const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
@@ -48,17 +48,64 @@ export default function NavbarWithDropdown({ auth, user }) {
               }
             </div> */}
           </div>
-        </div>
-      </div>
+          <div className="hidden sm:flex sm:items-center sm:ml-6">
+            <div className="ml-3 relative">
+              <Dropdown>
+                <Dropdown.Trigger>
+                  <span className="inline-flex rounded-md">
+                    <button
+                      type="button"
+                      className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                    >
+                      {user.name}
+                      <svg
+                        className="ml-2 -mr-0.5 h-4 w-4"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </button>
+                  </span>
+                </Dropdown.Trigger>
 
-      <div className={(showingNavigationDropdown ? 'block' : 'hidden') + ' sm:hidden'}>
-        <div className="pt-4 pb-1 border-t border-gray-200">
-          <div className="px-4">
-            <div className="font-medium text-base text-gray-800">{user.name}</div>
-            <div className="font-medium text-sm text-gray-500">{user.email}</div>
+                <Dropdown.Content>
+                  <Dropdown.Link href={route('profile.edit')}>Profile</Dropdown.Link>
+                  <Dropdown.Link href={route('logout')} method="post" as="button">
+                    Log Out
+                  </Dropdown.Link>
+                </Dropdown.Content>
+              </Dropdown>
+            </div>
           </div>
 
-          <div className="mt-3 space-y-1">
+          <div className="-mr-2 flex items-center sm:hidden">
+            <button
+              onClick={() => setShowingNavigationDropdown((previousState) => !previousState)}
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
+            >
+              <svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                <path
+                  className={!showingNavigationDropdown ? 'inline-flex' : 'hidden'}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+                <path
+                  className={showingNavigationDropdown ? 'inline-flex' : 'hidden'}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
           </div>
         </div>
       </div>
